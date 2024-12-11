a30c8dc5130a59431d3d79d09f1548731c8dd932
Liam Duggan <dugganl1@tcd.ie>
2024-12-11 00:29:09 +0000

    added ability to include test IP

--- functions/api/visitor-info.js
+++ functions/api/visitor-info.js
@@ -1,10 +1,17 @@
 export async function onRequest(context) {
   try {
-    const ip = context.request.headers.get("cf-connecting-ip");
+    // Get URL parameters
+    const url = new URL(context.request.url);
+    // Use test IP if provided in query param, otherwise use real visitor IP
+    const ip = url.searchParams.get("testip") || context.request.headers.get("cf-connecting-ip");
     const country = context.request.headers.get("cf-ipcountry");
 
     let companyData = null;
     if (context.env.IPINFO_TOKEN) {
+      // Test with known corporate IPs:
+      // Google: 8.8.8.8
+      // Microsoft: 20.112.52.29
+      // Amazon: 176.32.103.205
       const response = await fetch(
         `https://ipinfo.io/${ip}/company?token=${context.env.IPINFO_TOKEN}`
       );
